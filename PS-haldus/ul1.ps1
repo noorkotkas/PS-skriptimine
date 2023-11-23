2e3a6b9b4273924fa1c54ab57d8aaf13153128a8
noorkotkas <christofer.uffert@voco.ee>
2023-11-23 11:47:32 +0200

    user loomise skript

---
+++ PS-haldus/ul1.ps1
@@ -0,0 +1,28 @@
+﻿do {
+    $eesnimi = Read-Host "Sisesta oma eesnimi (ladina tähtedega):"
+    $perenimi = Read-Host "Sisesta oma perenimi (ladina tähtedega):"
+
+    $ladinaTahed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    $nimeError = $false
+
+    foreach ($t2ht in $eesnimi.ToCharArray()) {
+        if ($ladinaTahed -notcontains $t2ht) {
+            Write-Host "Viga! Eesnimi peab koosnema ainult ladina tähtedest."
+            $nimeError = $true
+            break
+        }
+    }
+
+    foreach ($t2ht in $perenimi.ToCharArray()) {
+        if ($ladinaTahed -notcontains $t2ht) {
+            Write-Host "Viga! Perenimi peab koosnema ainult ladina tähtedest."
+            $nimeError = $true
+            break
+        }
+    }
+
+} while ($nimeError)
+
+Write-Host "Eesnimi: $eesnimi"
+Write-Host "Perenimi: $perenimi"
