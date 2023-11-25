4abe938e99f202e9c68709c9c70b1c10dac3b914
noorkotkas <christofer.uffert@voco.ee>
2023-11-25 15:26:04 +0200

    kustutamisskript

---
+++ PS-haldus/ul2.ps1
@@ -0,0 +1,24 @@
+﻿# Funktsioon, mis kustutab kasutaja
+function Remove-MyUser {
+    param (
+        [string]$username
+    )
+
+    try {
+        $user = Get-LocalUser -Name $username -ErrorAction Stop
+        Remove-LocalUser -Name $user.Name -ErrorAction Stop
+        Write-Host "Kasutaja $username on edukalt kustutatud!"
+    } catch {
+        Write-Host "Viga kustutamisel: $_" -ForegroundColor Red
+    }
+}
+
+# Küsi kasutaja ees- ja perekonnanime
+$eesnimi = Read-Host "Sisesta kasutaja eesnimi (ladina tähtedega):"
+$perenimi = Read-Host "Sisesta kasutaja perenimi (ladina tähtedega):"
+
+# Koosta kasutajanimi
+$kasutajanimi = $eesnimi.ToLower() + $perenimi.ToLower()
+
+# Kustuta kasutaja
+Remove-MyUser -username $kasutajanimi
