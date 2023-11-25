05500a9f42b54f5286e156ace3ff51f554ddfd99
noorkotkas <christofer.uffert@voco.ee>
2023-11-25 13:54:49 +0200

    hrj5 lahendus

---
+++ PS-algajatele/harjutus5.ps1
@@ -0,0 +1,19 @@
+﻿# Get-Process command
+$notepadProcess = Get-Process -Name "notepad" | Select-Object ProcessName, Id
+$notepadProcess
+
+# Get-ChildItem command
+$testFiles = Get-ChildItem -Path "C:\temp\test"
+
+# Filter for CSV files
+$csvFiles = $testFiles | Where-Object { $_.Extension -eq ".csv" }
+
+# Display information for each CSV file
+foreach ($file in $csvFiles) {
+    $sizeinKB = $file.Length / 1KB
+    $sizeinMB = $file.Length / 1MB
+
+    Write-Host "`nFileName: $($file.Name)"
+    Write-Host "Size in KB: $($sizeinKB)"
+    Write-Host "Size in MB: $($sizeinMB)"
+}
